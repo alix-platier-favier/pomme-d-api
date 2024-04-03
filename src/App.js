cfdb35581ed37f3b3cb77896e5624e972dcfd74e
alixunderscore <alix.platier-favier@laplateforme.io>
2024-04-03 04:48:53 -0300

    pomme d'api fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import './App.css';
-import Homepage from './pages/homepage';
+import { createContext, useState } from "react";
+import "./App.css";
+import Homepage from "./pages/homepage";
 
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState(false);
+
+  console.log(theme);
+
   return (
-    <div className='App'>
-      <Homepage />
-    </div>
-  )
+    <ThemeContext.Provider
+      value={{
+        theme,
+        setTheme,
+      }}
+    >
+      <div className={`App ${theme ? 'addToggleClass' : ''}`}>
+        <Homepage />
+      </div>
+    </ThemeContext.Provider>
+  );
 }
 
 export default App;
